d7f55a28da917d8e0867ff654fc040d2779a9e5f
bmiche3 <bmiche3@gmail.com>
2014-10-25 14:46:08 -0700

    added traits to human file

--- src/raceCollection/Human.java
+++ src/raceCollection/Human.java
@@ -2,5 +2,12 @@ package raceCollection;
 import dndProject.RaceMenu;
 
 public class Human extends RaceMenu {
+	
+	// Human Traits
+	String abilityScoreIncrease = "Your ability scores each increase by 1.";
+	String alignment = "Humans tend toward no particular alignment. The best and worst are found among them.";
+	String size = "Humans vary widely in height and build, from barely 5 feet to well over 6 feet tall. Regardless of your position in that range, your size is Medium.";
+	String speed = "Your base walking speed is 30 feet.";
+	String languages = "You can speak, read, and write Common and one extra language of your choice. Humans typically learn the languages of other peoples they deal with, including obscure dialects. They are fond of sprinkling their speech with words borrowed from other tongues: Orc curses, Elvish musical expressions, Dwarvish military phrases, and so on.";
 
 }
